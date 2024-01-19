c08dbabaaec40e6277e764673286aba18a7934ec
DexterCapillan <146181493+DexterCapillan@users.noreply.github.com>
2024-01-20 06:24:45 +0800

    done

--- script.js
+++ script.js
@@ -1,11 +1,82 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const myButton = document.getElementById('myButton');
+function showAboutMePopup() {
+    var popup = document.getElementById('about-me-popup');
+    popup.style.display = 'block';
+    
+    document.body.style.backdropFilter = 'blur(5px)';
+    
+    setTimeout(function () {
+        popup.style.opacity = 1;
+    }, 10);
+}
 
-    myButton.addEventListener('click', function () {
-        const contactSection = document.getElementById('contact');
-        contactSection.style.backgroundColor = '#ffcc00';
-        setTimeout(() => {
-            contactSection.style.backgroundColor = '#fff';
-        }, 1000);
-    });
-});
+function closeAboutMePopup() {
+    var popup = document.getElementById('about-me-popup');
+    popup.style.opacity = 0;
+    document.body.style.backdropFilter = 'none';
+    setTimeout(function () {
+        popup.style.display = 'none';
+    }, 500); 
+}
+function showEducationPopup() {
+    var popup = document.getElementById('education-popup');
+    popup.style.display = 'block';
+    setTimeout(function () {
+        popup.style.opacity = 1;
+    }, 10);
+}
+
+function closeEducationPopup() {
+    var popup = document.getElementById('education-popup');
+    popup.style.opacity = 0;
+    setTimeout(function () {
+        popup.style.display = 'none';
+    }, 500); 
+}
+function showExperiencePopup() {
+    var popup = document.getElementById('experience-popup');
+    popup.style.display = 'block';
+    setTimeout(function () {
+        popup.style.opacity = 1;
+    }, 10);
+}
+
+function closeExperiencePopup() {
+    var popup = document.getElementById('experience-popup');
+    popup.style.opacity = 0;
+    setTimeout(function () {
+        popup.style.display = 'none';
+    }, 500); 
+
+}
+function showSkillsPopup() {
+    var popup = document.getElementById('skills-popup');
+    popup.style.display = 'block';
+    setTimeout(function () {
+        popup.style.opacity = 1;
+    }, 10);
+}
+
+function closeSkillsPopup() {
+    var popup = document.getElementById('skills-popup');
+    popup.style.opacity = 0;
+    setTimeout(function () {
+        popup.style.display = 'none';
+    }, 500); 
+
+}
+function showContactsPopup() {
+    var popup = document.getElementById('contacts-popup');
+    popup.style.display = 'block';
+    setTimeout(function () {
+        popup.style.opacity = 1;
+    }, 10);
+}
+
+function closeContactsPopup() {
+    var popup = document.getElementById('contacts-popup');
+    popup.style.opacity = 0;
+    setTimeout(function () {
+        popup.style.display = 'none';
+    }, 500); 
+
+}
